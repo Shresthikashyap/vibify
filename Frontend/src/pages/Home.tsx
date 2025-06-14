2b1c81b831991beac65233815048d64dfb8ce510
Arvind Keshri <arvidce@gmail.com>
2025-06-14 20:55:36 +0530

    removed overflow in main

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -5,13 +5,13 @@ import Footer from "../components/common/footer/Footer";
 
 const Home = () => {
   return (
-    <div className="min-h-screen overflow-hidden">
+    <div className="min-h-screen">
       <Navbar />
       <div className="grid grid-cols-6 ">
         <div className="col-span-1 grid mt-16">
           <Sidebar />
         </div>
-        <div className="col-span-5 grid overflow-auto mt-4">
+        <div className="col-span-5 grid mt-4">
           <MainContent />
         </div>
       </div>
